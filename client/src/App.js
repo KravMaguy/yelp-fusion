5ff19d306f4531c7d67e24eaec93cecf89c9a4bf
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2021-11-22 15:03:49 -0600

    res.json not a function

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import axios from "axios";
 function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    console.log("uese");
+    axios.get("/api").then((data) => {
+      console.log(data);
+      setData(data.message);
+    });
   }, []);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{!data ? "Loading here..." : data}</p>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-
+const axios = require("axios");
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  axios
+    .get(
+      "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    .then((res) => res.json(res.data))
+    .catch((err) => console.log("the error is: ", err.message));
 });
 
 app.listen(PORT, () => {
