aecdcef93bc6a610a349ed85eee7df6275176127
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2021-11-23 23:52:20 -0600

    search on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import Buisness from "./Buisness";
+
 function App() {
   const location = useLocation();
   const { pathname } = location;
+
+  const [term, setTerm] = useState("");
   const [data, setData] = useState([]);
-  React.useEffect(() => {
-    axios.get("/api").then(({ data }) => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("handlesubmit with your input: ", term);
+    try {
+      const terms = { term };
+      const { data } = await axios.post("/api/", terms);
+      console.log("recieved from backend: ", data);
       setData(data.businesses);
-    });
-  }, []);
+    } catch (error) {
+      // setIsError(true)
+      // setError(error.response.data.message)
+      console.log(error, "the error");
+    }
+  };
 
   return (
     <div className='App'>
       <header className='App-header'>
+        {pathname === "/" && (
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              onChange={(e) => setTerm(e.target.value)}
+              placeholder='Search in Naperville'
+              type='text'
+              name='name'
+            />
+            <input type='submit' value='Submit' />
+          </form>
+        )}
+
         {pathname !== "/" && (
           <Buisness name={pathname.slice(1).replace(/%20/g, " ")} data={data} />
         )}
+
         {pathname === "/" &&
           (data.length < 1 ? (
             <img src={logo} className='App-logo' alt='logo' />
--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
-require("dotenv").config();
-const PORT = process.env.PORT || 3001;
 const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
 const token = process.env.TOKEN;
 
-app.get("/api", (req, res) => {
+app.use("/api/", async (req, res, next) => {
+  const body = req.body;
+  const { term } = body;
   axios
     .get(
-      "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972",
+      "https://api.yelp.com/v3/businesses/search?term=" +
+        term +
+        "&location=Naperville",
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -19,6 +29,4 @@ app.get("/api", (req, res) => {
     .catch((err) => console.log("the error is: ", err.message));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+app.listen(port, () => console.log(`App is running on port ${port}`));
