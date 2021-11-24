17d5755913ac075d8f31cd321d954fa7b51a2474
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2021-11-24 14:25:37 -0600

    remove cors

---
+++ readme.md
@@ -0,0 +1,4 @@
+Instructions for usage:
+
+Rename env.example to .env
+Your fusion Api key should be defined in the .env file as the TOKEN variable
--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.json());
 const token = process.env.TOKEN;
 app.use("/api/", async (req, res, next) => {
