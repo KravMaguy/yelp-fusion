3ebff87b233e80d30b6574220b28b2f1567e6253
krav maguy <flex4lease@gmail.com>
2022-03-14 16:28:18 -0500

    only show on map if startDate is defaultDate

--- client/src/NavMap.js
+++ client/src/NavMap.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Map from "./Map";
 import { restaurantObjects } from "./utils.js";
 import { OverlayView } from "@react-google-maps/api";
@@ -8,30 +8,79 @@ import "react-day-picker/lib/style.css";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import dateFnsParse from "date-fns/parse";
 import { DateUtils } from "react-day-picker";
-
-function createKey({ location }) {
-  return location.lat + location.lng;
-}
+import { getNextDayOfTheWeek, weekDays } from "./utils.js";
+import { v4 as uuidv4 } from "uuid";
+const { add, isSameDay } = require("date-fns");
 
 const NavMap = ({ BuisnessData: data, center }) => {
   const mockBuisnessData = true;
   if (mockBuisnessData) {
     data = restaurantObjects;
   }
+
   const [childClicked, setChildClicked] = useState(null);
   const [defaultDate, setDefaultDate] = useState(new Date());
+  const [result, setResult] = useState([]);
+  useEffect(() => {
+    const result = data
+      .filter((buisness) => buisness.hours)
+      .flatMap((buisness) => {
+        console.log(buisness, "buisness");
+        const { hours } = buisness;
+        const shifts = hours[0].open.map((shift) => {
+          const shift_hours_start = [
+            parseInt(shift.start.slice(0, 2)),
+            parseInt(shift.start.slice(2)),
+          ];
 
-  const result = data.map((buisness) => {
-    return {
-      ...buisness,
-      name: buisness.name,
-      img: buisness.image_url,
-      location: {
-        lat: buisness.coordinates.latitude,
-        lng: buisness.coordinates.longitude,
-      },
-    };
-  });
+          const shift_hours_end = [
+            parseInt(shift.end.slice(0, 2)),
+            parseInt(shift.end.slice(2)),
+          ];
+          const start = new Date(
+            getNextDayOfTheWeek(weekDays[shift.day]).setHours(
+              shift_hours_start[0],
+              shift_hours_start[1],
+              0
+            )
+          );
+          let end = new Date(
+            getNextDayOfTheWeek(weekDays[shift.day]).setHours(
+              shift_hours_end[0],
+              shift_hours_end[1],
+              0
+            )
+          );
+          if (shift.is_overnight || shift.end === "0000") {
+            // console.log("reached the case to add 24 hours");
+            end = add(end, {
+              days: 1,
+            });
+          }
+
+          const Shift = {
+            id: uuidv4(),
+            title: buisness.name,
+            name: buisness.name,
+            img: buisness.image_url,
+            start,
+            end,
+            location: {
+              lat: buisness.coordinates.latitude,
+              lng: buisness.coordinates.longitude,
+            },
+            all_day:
+              shift_hours_end[0] + shift_hours_end[1] === 0 ? true : false,
+          };
+          return Shift;
+        });
+        return shifts;
+      });
+    const newResult = result.filter((shift) =>
+      isSameDay(defaultDate, shift.start)
+    );
+    setResult(newResult);
+  }, [data, defaultDate]);
 
   const onClick = (id) => {
     setChildClicked(id);
@@ -48,7 +97,6 @@ const NavMap = ({ BuisnessData: data, center }) => {
     }
     return undefined;
   }
-  console.log({ defaultDate });
   const FORMAT = "MM/dd/yyyy";
 
   return (
@@ -73,7 +121,7 @@ const NavMap = ({ BuisnessData: data, center }) => {
               <Map center={center}>
                 {result.map((location) => (
                   <OverlayView
-                    key={createKey(location)}
+                    key={uuidv4()}
                     position={{
                       lat: location.location.lat,
                       lng: location.location.lng,
